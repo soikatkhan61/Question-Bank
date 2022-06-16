320e2b862c4dce937c152b475472aa0abcc644eb
Soikat Hossain <soikatkhan61@gmail.com>
2022-06-16 12:14:56 -0400

    update database

--- backend/App.js
+++ backend/App.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
 const port = 3000 || process.env.port;
 
@@ -6,6 +7,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+let mongodb_uri
+
+if(true){
+  mongodb_uri = "mongodb+srv://admin1:admin1@cluster0.m2jxcwn.mongodb.net/test"
+}else{
+  mongodb_uri = "mongodb://localhost:27017"
+}
+
+mongoose.connect(mongodb_uri, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+  console.log("mongodb is connected");
+  app.listen(port,()=>{
+      console.log("app is listen on port " + port)
+  })
+}).catch((error)=>{
+  console.log("mondb not connected");
+  console.log(error);
 });
+
